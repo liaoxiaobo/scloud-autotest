a0c17e8c4eaf6b0c3c58c63b372d7d52fe7de9a6
caowl <caowl@mysugoncloud.com>
2026-06-18 16:56:27 +0800

    fix(storage): 临时移除 __init__.py 中未提交 SfsPage 的导入，避免 Jenkins 导入失败

--- sugon_web/pages/storage/__init__.py
+++ sugon_web/pages/storage/__init__.py
@@ -2,7 +2,6 @@ from .evs import EvsPage as VolumePage
 from .evss import EvssPage
 from .oss import OssPage
 from .recycle import RecyclePage
-from .sfs import SfsPage
 
 
 class EvsPage(VolumePage, RecyclePage, EvssPage):
@@ -10,4 +9,4 @@ class EvsPage(VolumePage, RecyclePage, EvssPage):
     service_name = "云硬盘"
 
 
-__all__ = ["EvsPage", "VolumePage", "RecyclePage", "EvssPage", "OssPage", "SfsPage"]
+__all__ = ["EvsPage", "VolumePage", "RecyclePage", "EvssPage", "OssPage"]
