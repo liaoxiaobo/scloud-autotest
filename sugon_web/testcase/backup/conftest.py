426d037ad50b2e555ae0164b62dda9b5566b40f3
xiangyang <xiangyang@sugon.com>
2026-07-06 20:15:34 +0800

    refactor(compute/ecs): 拆分 ecs_ssh 的 vm_pre_data 为单一职责辅助方法
    Signed-off-by: xiangyang <xiangyang@sugon.com>

--- sugon_web/testcase/backup/conftest.py
+++ sugon_web/testcase/backup/conftest.py
@@ -185,7 +185,7 @@ def _create_backup_vm_base_resources(
     create_request, count, network, subnet = _build_vm_create_request(request, vm_params, base_name)
     vm_names = _build_vm_fixture_names(create_request["basic"]["name"], count)
 
-    _allocate_eips(vpc_page, count=count)
+    # _allocate_eips(vpc_page, count=count)
     _create_vm_resources(
         ecs_page=ecs_page,
         create_request=create_request,
@@ -229,8 +229,7 @@ def _prepare_single_vm_backup_metadata(
         ecs_page.goto_service("弹性云服务器")
         ecs_page.goto_submenu("弹性云服务器")
         ecs_page.set_table_header("架构")
-        ecs_page.ecs_bind_pub_ip(vm_data["name"])
-        ecs_page.assert_popup_success("执行成功")
+
         row_data = ecs_page.get_row_data(vm_data["name"])
         arch = row_data.get("架构")
         mfip = bind_vm_mfip(ecs_page, ssh_host, browser, config, vm_data["name"])
